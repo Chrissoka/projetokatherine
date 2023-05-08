9549997b0be0b373d3f18dea10c616554078c212
Chrissoka <christianediassp@gmail.com>
2023-05-08 20:52:07 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,64 +1,15 @@
-var el = document.querySelector("#text");
-var text = "Mulheres podem fazer tudo que os homens fazem, mas mulheres possuem maior capacidade de imaginar - Katherine C. G. Johnson"
-var interval = 200;
-function showtext(el, text, interval) {
-  var char = text.split("").reverse();
-  
-  var typer = setInterval(function() {
-  
-    if (!char.length) {
-        return clearInterval(typer)
-    }
-    
-    var next = char.pop();
-    
-    el.innerHTML += next;
-    
-  }, interval);
-  
-}
-showtext(el, text, interval);
+const efeito = document.querySelector(".efeito");
 
-const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
-
-function getDistanceFromTheTop(element) {
-  const id = element.getAttribute("href");
-  return document.querySelector(id).offsetTop;
+function digitacao(texto, contador) {
+  if (contador < texto.length) {
+    setTimeout(() => {
+      efeito.textContent += texto.charAt(contador);
+      contador++;
+      digitacao(texto, contador);
+    }, 89);
+  }
 }
 
-function scrollToSection(event) {
-  event.preventDefault();
-  const distanceFromTheTop = getDistanceFromTheTop(event.target) - 90;
-  smoothScrollTo(0, distanceFromTheTop);
-}
+digitacao("Mulheres podem fazer tudo que os homens fazem, mas mulheres possuem maior capacidade de imaginar - Katherine C. G. Johnson", 0);
 
-menuLinks.forEach((link) => {
-  link.addEventListener("click", scrollToSection);
-});
-
-function smoothScrollTo(endX, endY, duration) {
-  const startX = window.scrollX || window.pageXOffset;
-  const startY = window.scrollY || window.pageYOffset;
-  const distanceX = endX - startX;
-  const distanceY = endY - startY;
-  const startTime = new Date().getTime();
-
-  duration = typeof duration !== "undefined" ? duration : 400;
-
-  const easeInOutQuart = (time, from, distance, duration) => {
-    if ((time /= duration / 2) < 1)
-      return (distance / 2) * time * time * time * time + from;
-    return (-distance / 2) * ((time -= 2) * time * time * time - 2) + from;
-  };
-
-  const timer = setInterval(() => {
-    const time = new Date().getTime() - startTime;
-    const newX = easeInOutQuart(time, startX, distanceX, duration);
-    const newY = easeInOutQuart(time, startY, distanceY, duration);
-    if (time >= duration) {
-      clearInterval(timer);
-    }
-    window.scroll(newX, newY);
-  }, 1000 / 60);
-}
 
